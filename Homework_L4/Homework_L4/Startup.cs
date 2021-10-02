4e3ce318fe50fe5c3d5476742b6af3d603c4bcc0
sergeybiophys <rove2hati@gmail.com>
2021-10-02 10:39:53 +0300

    project setup

--- Homework_L4/Homework_L4/Startup.cs
+++ Homework_L4/Homework_L4/Startup.cs
@@ -1,3 +1,4 @@
+using Hubs.Homework_L4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace Homework_L4
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +46,8 @@ namespace Homework_L4
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<GameHub>
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
